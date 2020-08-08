8faf782e2d2344075ec47ab72714dc3e60553086
Dedal378 <orlovetsfoto@gmail.com>
2020-08-08 03:16:53 +0300

    обработка ошибок, add index.js in store

--- store/auth.js
+++ store/auth.js
@@ -1,5 +1,5 @@
 export const state = () => ( {
-  token: null
+  token: true
 } )
 
 export const mutations = {
@@ -13,15 +13,20 @@ export const mutations = {
 
 export const actions = {
   async login ({ commit, dispatch }, formData) {
-    const token = await new Promise(resolve => {
-      setTimeout(() => resolve('mock-token'), 2000)
-    })
-    dispatch('setToken', token)
+    try {
+      const token = await new Promise((resolve, reject) => {
+        setTimeout(() => resolve('mock-token'), 2000)
+      })
+      dispatch('setToken', token)
+    } catch (e) {
+      commit('setError', e, { root: true })
+      throw e
+    }
   },
-  setToken ({commit}, token) {
+  setToken ({ commit }, token) {
     commit('setToken', token)
   },
-  logout ({commit}) {
+  logout ({ commit }) {
     commit('clearToken')
   }
 }
